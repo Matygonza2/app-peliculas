b31606f94fb07a72aad9dd708c3410025f881ecd
Luis IFTS <94791421@ifts24.edu.ar>
2026-07-01 15:24:40 -0300

    integrar FavoritosService en detalle para agregar favoritos

--- src/app/pages/detalle/detalle.component.ts
+++ src/app/pages/detalle/detalle.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DetalleService } from '../../services/detalle.service';
 import { Pelicula, PeliculaDetalle } from '../../model/pelicula';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FavoritosService } from '../../services/favoritos.service';
 
 @Component({
   selector: 'app-detalle',
@@ -11,13 +12,13 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 })
 
 export class DetalleComponent implements OnInit {
-
   peliculas: Pelicula[] = [];
   peliculaSeleccionada?: PeliculaDetalle;
 
   constructor(
     private _detalleService: DetalleService,
-    private _route: ActivatedRoute
+    private _route: ActivatedRoute,
+    private favoritosService: FavoritosService
   ) { }
 
   ngOnInit(): void {
@@ -33,5 +34,36 @@ export class DetalleComponent implements OnInit {
       },
       error: error => console.log(error)
     })
+  }
+
+  agregarALista(): void {
+    if (!this.peliculaSeleccionada) {
+      console.warn('No hay película seleccionada');
+      return;
+    }
+
+    const p = this.peliculaSeleccionada;
+    const peliculaParaFavorito: Pelicula = {
+      id: p.id,
+      title: p.title,
+      poster_path: p.poster_path,
+      release_date: new Date(p.release_date),
+      vote_average: p.vote_average,
+      adult: false,
+      backdrop_path: p.backdrop_path,
+      genre_ids: p.genres.map(g => g.id),
+      original_language: p.original_language,
+      original_title: p.original_title,
+      overview: p.overview,
+      popularity: p.popularity,
+      video: false,
+      vote_count: 0,
+      softcore: false
+    };
+
+    this.favoritosService.agregarFavorito(peliculaParaFavorito).subscribe({
+      next: () => console.log('Agregada a favoritos'),
+      error: (err) => console.error('Error al agregar:', err)
+    });
   }
 }
