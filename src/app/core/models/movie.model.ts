0514cbde0b9cb8d721964c9bfd4043155c9143fd
42930037-ifts <42930037@ifts24.edu.ar>
2026-06-30 23:30:45 -0300

    Agregar interfaces MovieListResponse y GenreListResponse

--- src/app/core/models/movie.model.ts
+++ src/app/core/models/movie.model.ts
@@ -11,3 +11,35 @@ export interface Genre {
   id: number;
   name: string;
 }
+export interface MovieListResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+}
+
+export interface GenreListResponse {
+  genres: Genre[];
+}
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  vote_average: number;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface MovieListResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+}
+
+export interface GenreListResponse {
+  genres: Genre[];
+}
